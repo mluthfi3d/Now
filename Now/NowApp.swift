21cf8b87abb063e7557750a5a719842968363504
mluthfi3d <mluthfi3d@gmail.com>
2023-07-17 14:08:43 +0700

    Initial Commit

---
+++ Now/NowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowApp.swift
+//  Now
+//
+//  Created by Muhammad Luthfi on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
